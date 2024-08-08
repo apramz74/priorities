9e09d8cc3f20044cc8de25229574e70d28adaf0e
Adam Pramono <apramono74@gmail.com>
2024-08-07 21:35:03 -0400

    Updated my old nav component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        indigo: {
+          100: "#e0e7ff",
+          600: "#4f46e5",
+          700: "#4338ca",
+        },
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      fontWeight: {
+        thin: "100",
+        extralight: "200",
+        light: "300",
+        normal: "400",
+        medium: "500",
+        semibold: "600",
+        bold: "700",
+        extrabold: "800",
+        black: "900",
+      },
+    },
   },
   plugins: [],
 };
